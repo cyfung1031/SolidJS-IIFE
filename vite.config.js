068639f1884ff387c2e5e34ab9a4bf9b90ff2dc4
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2024-12-13 01:37:29 +0900

    v013

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,31 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  build: {
-    lib: {
-      entry: 'src/main.js',
-      name: 'SolidJS',
-      fileName: 'solid-js'
+export default [defineConfig({
+    build: {
+      minify: false,
+      lib: {
+        entry: 'src/main.js',
+        name: 'SolidJS',
+        fileName: 'solid-js-dev'
+      }
+      // Default outDir is "dist"
+      // You can customize other options if needed
     }
-    // Default outDir is "dist"
-    // You can customize other options if needed
-  }
-});
+  }),defineConfig({
+    build: {
+      minify: "terser",
+      target: "esnext",
+      cssCodeSplit: false,
+      terserOptions: {
+        keep_classnames: true,
+        keep_fnames: true,
+      },
+      lib: {
+        entry: 'src/main.js',
+        name: 'SolidJS',
+        fileName: 'solid-js-prod'
+      }
+      // Default outDir is "dist"
+      // You can customize other options if needed
+    }
+  })];
