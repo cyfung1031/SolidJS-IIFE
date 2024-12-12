efdb89f6d3ce1a0516597e72294f7bab25fb7030
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2024-12-13 01:18:31 +0900

    v006

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
+    lib: {
+      entry: 'src/main.js',
+      name: 'SolidJS',
+      fileName: 'solid-js'
+    }
     // Default outDir is "dist"
     // You can customize other options if needed
   }
